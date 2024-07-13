69dde17bd68c9395c195d477b38290d8b5c91107
throbbey <158000850+throbbey@users.noreply.github.com>
2024-07-13 11:04:50 -0700

    Update app.py
    removed dotenv

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
 import dropbox
 from datetime import datetime
 import fitz  # PyMuPDF
 
-load_dotenv()
 
 APP_KEY = os.getenv('APP_KEY')
 APP_SECRET = os.getenv('APP_SECRET')
